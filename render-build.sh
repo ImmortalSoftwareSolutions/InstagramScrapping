668e6ff37a067323f12d90c4562cca770db5e712
BhoomiAgrawal12 <bhoomiagrawal1212@gmail.com>
2025-04-02 19:58:01 +0530

    Added render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,17 +1,6 @@
-#!/usr/bin/env bash
-set -eux
+# Install Python dependencies
+pip install -r requirements.txt
 
-# Install missing dependencies for Playwright
-apt-get update && apt-get install -y \
-  libgtk-4-1 \
-  libgraphene-1.0-0 \
-  libgstgl-1.0-0 \
-  libgstcodecparsers-1.0-0 \
-  libavif15 \
-  libenchant-2-2 \
-  libsecret-1-0 \
-  libmanette-0.2-0 \
-  libgles2
-
-# Install Playwright browsers
-npx playwright install
+# Install Playwright & Chromium
+pip install playwright
+playwright install --with-deps chromium
